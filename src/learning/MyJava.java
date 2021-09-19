335bfc4803a8e7fb633c10bfac02b1abac600892
Omkar Singh <omkars077@gmail.com>
2021-09-18 20:44:51 -0700

    Chapter1

---
+++ src/learning/MyJava.java
@@ -0,0 +1,11 @@
+package learning;
+
+public class MyJava {
+
+    public static void main(String[] args){
+        
+        System.out.println("This is a java program : "+"And based on coding.");
+        System.out.println("This is my first program . ");
+        System.out.println("My First Experience");
+    }
+}
