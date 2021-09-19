65b471cfe0f7ed9ae254544f5249d4c6addda641
Omkar Singh <omkars077@gmail.com>
2021-09-18 20:54:36 -0700

    variable class

--- src/learning/MyJava.java
+++ src/learning/MyJava.java
@@ -3,9 +3,15 @@ package learning;
 public class MyJava {
 
     public static void main(String[] args){
-        
-        System.out.println("This is a java program : "+"And based on coding.");
-        System.out.println("This is my first program . ");
-        System.out.println("My First Experience");
+
+        String brand="Samsung";
+        int ram=4;
+        int battery=5000;
+        double price=12000.5;
+
+        System.out.println("This is the basic features of every mobile : "+brand);
+        System.out.println("This mobile price is : "+price+" rs"+".");
+        System.out.println("Samsung battery backup is : "+battery+"mah");
+        System.out.println("Ram of this mobile is : "+ram+" gb");
     }
 }
